9ca8670a1c9c984788399dcc64ee898f449fc2e5
KinMengNg <85046928+KinMengNg@users.noreply.github.com>
2023-01-25 17:42:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 from emotion_classifier import lr_model
 
 lr_model1 = lr_model( 'emotion_classifier_pipeline_lr.pkl')
-lr_model1.predict("Slay")
+
+text = ''
+while text != 'quit':
+  text = input("Enter a text prompt: ")
+  
+  if text == 'quit':
+    break
+    
+  else:
+    print(lr_model1.predict(text))
