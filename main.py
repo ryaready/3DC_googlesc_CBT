8a083dfef8ccd3b6ad4efecc42c939f95fb0afdb
KinMengNg <85046928+KinMengNg@users.noreply.github.com>
2023-01-25 17:51:27 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from emotion_classifier import lr_model
 
+#Added the print statements because the loading takes awhile
+print("Loading model...")
 lr_model1 = lr_model( 'emotion_classifier_pipeline_lr.pkl')
+print("Done!")
+print()
 
-text = ''
-while text != 'quit':
+while True:
   text = input("Enter a text prompt: ")
   
   if text == 'quit':
     break
     
   else:
-    print(lr_model1.predict(text))
+    most_likely_emotion, probability_distribution = lr_model1.predict(text)
+    print('Emotion: ', most_likely_emotion)
+    print('Emotion Probability Distribution: ', probability_distribution)
+    print()
